96651ebea2029ae1ebb2cb51100359b666b9ba4f
Ayushi Yadav <107626308+ayushii15@users.noreply.github.com>
2022-09-11 10:40:51 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How Many Days are there in a week?",
+    answer: "7",
+    options: [
+      "6",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many days are there in a month?",
+    answer: "31",
+    options: [
+      "21",
+      "31",
+      "41",
+      "51"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many days are there in a year?",
+    answer: "365",
+    options: [
+      "165",
+      "265",
+      "365",
+      "465"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is our national anthem?",
+    answer: "Jan Gan Man",
+    options: [
+      "Vande Matram",
+      "Ma tujhe salam",
+      "I love my india",
+      "Jan Gan Man"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is our national animal?",
+    answer: "Tiger",
+    options: [
+      "Elephant",
+      "Lion",
+      "Tiger",
+      "Leopard"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is our national bird",
+    answer: "Peacock",
+    options: [
+      "Sparrow",
+      "Pigeon",
+      "Peacock",
+      "Parrot"
+    ]
+  },
+  {
+    numb: 7,
+    question: "When do we celebrate our Independance Day?",
+    answer: "On 15th Of August",
+    options: [
+      "On 15th Of August",
+      "On 15th Of September",
+      "On 15th Of January",
+      "On 15th Of October"
+    ]
+  },
+  {
+    numb: 8,
+    question: "When do we celebrate our Republic Day",
+    answer: "On 26th of January",
+    options: [
+      "On 26th of December",
+      "On 26th of January",
+      "On 26th of August",
+      "On 26th of September"
+    ]
+  },
+  {
+    numb: 9,
+    question: "When do we celebrate Teacher's Day?",
+    answer: "On 5th of September",
+    options: [
+      "On 5th of November",
+      "On 5th of October",
+      "On 5th of September",
+      "On 5th of March"
+    ]
+  },
+  {
+    numb: 10,
+    question: "When do we celebrate Children's Day?",
+    answer: "On 14th of November",
+    options: [
+      "On 14th of November",
+      "On 14th of December",
+      "On 14th of May",
+      "On 14th of June"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
